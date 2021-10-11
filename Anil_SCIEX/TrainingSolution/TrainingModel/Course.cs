a3b254c5305bd2cc72aee576b330ca1ed0f1ac07
Anil Kumar S <anilkumar.s@sciex.com>
2021-10-11 13:36:51 +0530

    code checkout

--- Anil_SCIEX/TrainingSolution/TrainingModel/Course.cs
+++ Anil_SCIEX/TrainingSolution/TrainingModel/Course.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrainingModel
+﻿namespace TrainingModel
 {
 	public class Course
 	{
+
 		public int Id { get; set; }
 		public string Name { get; set; }
 	}
