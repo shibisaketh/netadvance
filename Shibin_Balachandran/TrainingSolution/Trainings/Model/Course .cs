4cf0a21ae5bdd18066051707b1ee1d300dd5ff42
shibin- <shibisaketh@gmail.com>
2021-10-11 22:17:18 +0530

    :Added  changes from Shibin LMS

---
+++ Shibin_Balachandran/TrainingSolution/Trainings/Model/Course .cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TrainingModel
+{
+    public class Course
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public void SetId(Guid id)
+        {
+            Id = id;
+        }
+        public Course( string courseName)
+        {
+            Name = courseName;
+            Id = new Guid();
+            Id = Guid.NewGuid();
+        }
+
+    }
+}
---
+++ Shibin_Balachandran/TrainingSolution/Trainings/Model/Training.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrainingModel
+{
+    public class Training
+    {
+        public Training(string name, string hr, long cost, Guid courseId)
+        {
+            Id = new Guid(Guid.NewGuid().ToString());
+            courseId = new Guid();
+            Name = name;
+            TrainingHr = hr;
+            Cost = cost;
+            courseId = CourseId;
+        }
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public string TrainingHr { get; set; }
+        public long Cost { get; set; }
+        Guid CourseId {get; }
+    }
+}
