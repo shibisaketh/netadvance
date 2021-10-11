2955e0c07dba55751408ca4b82d8a66205e0fbd7
NETADDS\harisairaju.aitam <harisairaju.aitam@sciex.com>
2021-10-11 16:24:55 +0530

    Added classes

---
+++ Hari_Sciex/TrainingSolution/TrainingModel/Training.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingModel
+{
+	class Training
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public double TrainingHr { get; set; }
+		public double Cost { get; set; }
+
+
+	}
+}
